b33c5436502c52e8599b6647c9296b28ec3f4b23
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-06-27 21:52:16 +0300

    updating

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,20 +1,19 @@
 import React from 'react';
-
-
-class HornedBeasts extends React.Component{
-    render(){
-        return(
+class HornedBeasts extends React.Component {
+    render() {
+        return (
             <div className="hornedbeasts">
                 <h2>
-                 {this.props.title}
+                    {this.props.title}
                 </h2>
 
-                <img src ={this.props.url} alt ={this.props.alt}/>
+                <img src={this.props.url} alt={this.props.alt} />
+                
                 <p>
                     {this.props.prag}
                 </p>
             </div>
-            
+
         )
     }
 }
