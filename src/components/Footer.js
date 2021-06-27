b56bce1043ea0f8cd28dfe8d36334ce6e77282b9
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-06-27 17:40:56 +0300

    Solving Lab-01

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div className="footer">
+                <span>
+                   CopyRighrt © Ibrahim Khdairat - 2021
+                </span>
+            </div>
+
+
+        )
+    }
+}
+
+export default Footer;
