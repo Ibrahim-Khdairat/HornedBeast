fe74e960a61513f6a01c8eaa412b367659227fc2
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-06-28 13:27:15 +0300

    bootstrabp

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,26 +1,27 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
 class HornedBeasts extends React.Component {
 
-    constructor(props)
-    {
+    constructor(props) {
         super(props);
         this.state = {
-            numOfHorned : 0
+            numOfHorned: 0
         }
 
-      
+
     }
-    increaseHorne = () =>{
+    increaseHorne = () => {
         this.setState({
-            numOfHorned : this.state.numOfHorned + 1
+            numOfHorned: this.state.numOfHorned + 1
         })
     }
-  
+
 
     render() {
         return (
             <div className="hornedbeasts">
-                <h2>
+                {/* <h2>
                     {this.props.title}
                 </h2>
 
@@ -31,7 +32,21 @@ class HornedBeasts extends React.Component {
                 </p>
                 <p>
                 💙 {this.state.numOfHorned}
-                </p>
+                </p> */}
+
+                <Card style={{ width: '18rem' }}>
+                  
+                    <Card.Body>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Img onClick={this.increaseHorne} variant="top" src={this.props.url} alt={this.props.title} />
+                        <Card.Text>
+                            {this.props.prag}
+                        </Card.Text>
+                        <Card.Text>
+                        💙 {this.state.numOfHorned}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
             </div>
 
         )
