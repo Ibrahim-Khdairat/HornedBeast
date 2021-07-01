0577a54ac69256440ee65d4a9bc11ddd732c109f
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-07-01 09:13:42 +0300

    Update the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends React.Component{
     return(
       <div className="header">
                 
-        <h1>Lab-01 : HornedBeasts</h1>
+        <h1>HornedBeasts</h1>
 
       </div>
     );
